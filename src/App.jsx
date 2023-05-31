ca6c4624f92aa161fed8f09948fb6e10da462f55
AlexSchmale <alexschmale2911@gmail.com>
2023-05-31 20:23:21 -0300

    entrega primer examen

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,76 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./Components/Card";
 
-import './App.css'
-import Card from './Components/Card'
+
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [color, setColor] = useState("");
+  const [datos, setDatos] = useState({});
+  const [error, setError] = useState(false)
+  const [mostrarTarjeta, setMostrarTarjeta] = useState(false)
+
+  function handleSubmit(e) {
+    let nombreSinEspacios = nombre.trim();
+
+    e.preventDefault();
+    
+    if (nombreSinEspacios.length > 3 && color.length === 6) {
+      
+      setDatos({ nombre, color});
+      console.log(nombre, color);
+      setError(false)
+      setMostrarTarjeta(true)
+      setNombre("");
+      setColor("");
+    }else{
+      setError(true)
+      setMostrarTarjeta(false)
+      console.log('error');
+    }
+  }
 
   return (
-    <div className='app'>
-    <h1>Carga de estudiantes</h1>
-    <form>
-
-    </form>
-      <Card />
+    <div className="app">
+      <h1>Completar formulario</h1>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "350px",
+          gap: "10px",
+          border: "1px solid purple",
+          borderRadius: '10px',
+          boxShadow: 'rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px, rgba(240, 46, 170, 0.05) 25px 25px'
+        }}
+      >
+        <label style={{ margin: "auto", marginTop:'10px'}}>Ingresar nombre:</label>
+        <input
+          onChange={(e) => setNombre(e.target.value)}
+          style={{ width: "200px", margin: "auto" }}
+          type="text"
+          placeholder="nombre"
+          value={nombre}
+        />
+        <label style={{ margin: "auto" }}>Ingrese color:</label>
+        <input
+          onChange={(e) => setColor(e.target.value)}
+          style={{ width: "200px", margin: "auto" }}
+          type="text"
+          placeholder="000000"
+          value={color}
+        />
+        <button type="submit" style={{ width: "100px", margin: "auto", marginBottom:'10px' }}>
+          Enviar
+        </button>
+        {error && <p style={{alignItems:'center',color:'red',  width:'240px', margin:'auto', padding:'auto'}}>Por favor chequea que la información sea correcta</p>}
+        
+      </form>
+      {mostrarTarjeta && <Card datos ={datos} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Card.css'
 
-const Card = () => {
+
+const Card = (props) => {
+
   return (
-    <div>Card</div>
+    <div className='contenedor-card'>
+      <h2>Tu nombre es: <span>{props.datos.nombre}</span></h2>
+      <p style={{border:'1px solid white', padding:'16px', backgroundColor:'#' + props.datos.color}}>Tu color favorito es: #{props.datos.color}</p> 
+    </div>
   )
 }
 
